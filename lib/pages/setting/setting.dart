a0389a2d46ed20212ebad078a8a05be2d4f2dca2
temp <245646299@qq.com>
2021-01-03 21:36:48 +0800

    修复APP名错别字，增加SSH终端

--- lib/pages/setting/setting.dart
+++ lib/pages/setting/setting.dart
@@ -1,5 +1,6 @@
 import 'package:dsm_helper/pages/provider/dark_mode.dart';
 import 'package:dsm_helper/pages/setting/about.dart';
+import 'package:dsm_helper/pages/terminal/terminal.dart';
 import 'package:dsm_helper/util/function.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -614,7 +615,38 @@ class _SettingState extends State<Setting> {
                       width: 20,
                     ),
                     Expanded(
-                      flex: 2,
+                      child: NeuButton(
+                        onPressed: () {
+                          Navigator.of(context).push(CupertinoPageRoute(builder: (context) {
+                            return Ssh();
+                          }));
+                        },
+                        // margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        decoration: NeumorphicDecoration(
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        bevel: 20,
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "assets/icons/ssh.png",
+                              width: 40,
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              "终端",
+                              style: TextStyle(fontSize: 16),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
                       child: Container(),
                     ),
                     SizedBox(
---
+++ lib/pages/terminal/terminal.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:xterm/flutter.dart';
+import 'package:xterm/theme/terminal_theme.dart';
+import 'package:xterm/theme/terminal_themes.dart';
+import 'package:xterm/xterm.dart';
+import 'package:dartssh/client.dart';
+
+class Ssh extends StatefulWidget {
+  @override
+  _SshState createState() => _SshState();
+}
+
+class _SshState extends State<Ssh> {
+  Terminal terminal;
+  SSHClient client;
+  String host = "http://192.168.0.233:22";
+  String username = "root";
+  String password = "yaoshuwei123";
+  @override
+  void initState() {
+    terminal = Terminal(onInput: onInput, theme: TerminalThemes.whiteOnBlack);
+    connect();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    terminal.close();
+    super.dispose();
+  }
+
+  void onInput(String input) {
+    client?.sendChannelData(utf8.encode(input));
+  }
+
+  void connect() {
+    terminal.write('connecting $host...');
+    client = SSHClient(
+      hostport: Uri.parse(host),
+      login: username,
+      print: print,
+      termWidth: 80,
+      termHeight: 25,
+      termvar: 'xterm-256color',
+      getPassword: () => utf8.encode(password),
+      response: (transport, data) {
+        terminal.write(data);
+      },
+      success: () {
+        terminal.write('connected.\n');
+      },
+      disconnected: () {
+        terminal.write('disconnected.');
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("终端"),
+      ),
+      body: TerminalView(
+        terminal: terminal,
+        onResize: (width, height) {
+          client?.setTerminalWindowSize(width, height);
+        },
+      ),
+    );
+  }
+}
